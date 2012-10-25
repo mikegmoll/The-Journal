6461caebec00b1cf8b3bd3c09939237da109f1ff
mikegmoll <mike.g.moll@gmail.com>
2012-10-25 03:53:00 -0700

    1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Triharder::Application.config.secret_token = '2096667104a61215d704675ec9966e940f619ea455ad0365edbd5d32d9922a1a8b2eb53a81d11fcba293c12bc61493a712be11623e420cbea73036188c403827'
